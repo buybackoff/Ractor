d5f54f4e0e98a723e1c94fa8c15adf775c59fb65
buybackoff <vbaybekov@gmail.com>
2015-07-17 15:00:24 +0300

    Readable Validation exceptions on DataContext

--- src/Ractor.Persistence/DynamicContexts.cs
+++ src/Ractor.Persistence/DynamicContexts.cs
@@ -9,6 +9,7 @@ using System.Data.Entity.ModelConfiguration.Conventions;
 using System.Diagnostics;
 using System.Linq;
 using System.Collections.Generic;
+using System.Data.Entity.Validation;
 
 namespace Ractor
 {
@@ -97,7 +98,29 @@ namespace Ractor
 		{
 			return new DataContext("Ractor");
 		}
-	}
+
+        //http://stackoverflow.com/questions/15820505/dbentityvalidationexception-how-can-i-easily-tell-what-caused-the-error
+
+        public override int SaveChanges() {
+            try {
+                return base.SaveChanges();
+            } catch (DbEntityValidationException ex) {
+                // Retrieve the error messages as a list of strings.
+                var errorMessages = ex.EntityValidationErrors
+                        .SelectMany(x => x.ValidationErrors)
+                        .Select(x => String.Format("Property: {0} Error: {1}", x.PropertyName, x.ErrorMessage));
+
+                // Join the list to a single string.
+                var fullErrorMessage = string.Join("; ", errorMessages);
+
+                // Combine the original exception message with the new one.
+                var exceptionMessage = string.Concat(ex.Message, " The validation errors are: ", fullErrorMessage);
+
+                // Throw a new DbEntityValidationException with the improved exception message.
+                throw new DbEntityValidationException(exceptionMessage, ex.EntityValidationErrors);
+            }
+        }
+    }
 
 
 
@@ -170,7 +193,30 @@ namespace Ractor
                 migrationMethod.Invoke(o, new object[] { name, migrationConfig });
             }
 		}
-	}
+
+
+        //http://stackoverflow.com/questions/15820505/dbentityvalidationexception-how-can-i-easily-tell-what-caused-the-error
+
+        public override int SaveChanges() {
+            try {
+                return base.SaveChanges();
+            } catch (DbEntityValidationException ex) {
+                // Retrieve the error messages as a list of strings.
+                var errorMessages = ex.EntityValidationErrors
+                        .SelectMany(x => x.ValidationErrors)
+                        .Select(x => String.Format("Property: {0} Error: {1}", x.PropertyName, x.ErrorMessage));
+
+                // Join the list to a single string.
+                var fullErrorMessage = string.Join("; ", errorMessages);
+
+                // Combine the original exception message with the new one.
+                var exceptionMessage = string.Concat(ex.Message, " The validation errors are: ", fullErrorMessage);
+
+                // Throw a new DbEntityValidationException with the improved exception message.
+                throw new DbEntityValidationException(exceptionMessage, ex.EntityValidationErrors);
+            }
+        }
+    }
 
 
 
